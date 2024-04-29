5d9563e5e537e3678fea316fc8fcc11ad7c6ceb3
NickLther88 <nlesage@theethercompany.com>
2024-04-29 17:08:10 +0200

    add windPlayer

--- Auditorium/Assets/Scripts/MusicBoxManager.cs
+++ Auditorium/Assets/Scripts/MusicBoxManager.cs
@@ -65,8 +65,6 @@ public class MusicBoxManager : MonoBehaviour
         {
             audioSource.volume += volumeUp;
             BarVolumeUpdate(audioSource.volume);
-
-            Destroy(collision.gameObject);
         }
     }
 }
